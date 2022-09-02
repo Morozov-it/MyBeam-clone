daca018b19e79580ee182c1a43cab6eb949d76af
defpmportal.2.0 <morozov.it2020@gmail.com>
2022-09-02 14:46:51 +0300

    Change constants

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,3 +1,3 @@
 export const STORAGE_TOKEN_KEY = process.env.REACT_APP_STORAGE_TOKEN_KEY || 'accessToken'
 export const STORAGE_USER_KEY = process.env.REACT_APP_STORAGE_USER_KEY || 'user'
-export const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/'
+export const baseURL = process.env.REACT_APP_BASE_URL || 'http://10.6.7.35:5000/'
